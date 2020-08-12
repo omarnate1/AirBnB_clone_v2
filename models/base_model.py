b4b2deca43c488564f909f1f352c7f3bc548753c
luismvargasg <1610@holbertonschool.com>
2020-08-12 05:32:11 +0000

    fixing __init__ not working setattr

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 
@@ -18,7 +19,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -27,9 +27,7 @@ class BaseModel:
                 if k in ['created_at', 'updated_at']:
                     self.__dict__[k] = datetime\
                                        .strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
-                elif k == 'id':
-                    self.id = v
-                else:
+                elif "__class__" not in k:
                     setattr(self, k, v)
 
     def __str__(self):
@@ -45,7 +43,7 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""       
+        """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
