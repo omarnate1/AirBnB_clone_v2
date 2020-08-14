8463d46bec4c7ec17770a5edb4e6e5c559cc4ad4
luismvargasg <1610@holbertonschool.com>
2020-08-14 00:37:51 +0000

    adding conditional to ask is _sa_instance_state exist in the dict

--- models/base_model.py
+++ models/base_model.py
@@ -57,7 +57,8 @@ class BaseModel():
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary.pop("_sa_instance_state")
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
