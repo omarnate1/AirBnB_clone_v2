5966473b3c3da0f87556ce22bbad652d15c9dcd1
Robinson Montes <1574@holbertonschool.com>
2020-08-18 05:21:22 +0000

    Last commit before to sleep

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
-"""Script (based on the file 1-pack_web_static.py) that distributes an
-archive to your web servers, using the function do_deploy.
+# -*- coding: utf-8 -*-
 """
-from datetime import datetime as dt
-from fabric.api import *
-import os
+Created on Mon Aug 13 14:21:54 2020
+@author: Robinson Montes
+"""
+from fabric.api import local, put, run, env
+from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ["35.227.35.75", "100.24.37.33"]
+env.hosts = ['35.227.35.75', '100.24.37.33']
 
 
 def do_pack():
-    """File compressor"""
-    try:
-        timestamp = dt.now().strftime("%Y%m%d%H%M%S")
-        filepath = "./versions/web_static_{}".format(timestamp)
-        local('mkdir -p ./versions')
-        local('tar -cvzf {}.tgz web_static'.format(filepath))
-        archive_path = "{}.tgz".format(filepath)
-        if os.path.exists(archive_path):
-            return archive_path
-    except:
+    """
+    Targging project directory into a packages as .tgz
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
         return None
 
 
 def do_deploy(archive_path):
-    """Deploy the files to the servers"""
-    if os.path.exists(archive_path):
-        cloudpath = "/data/web_static/releases/" + archive_path[:-4]
+    """Deploy the boxing package tgz file
+    """
+    try:
         archive = archive_path.split('/')[-1]
-        current = "/data/web_static/current"
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
         put(archive_path, '/tmp')
-        run("mkdir -p {}".format(cloudpath))
-        run("tar -xzf /tmp/{} -C {}".format(archive, cloudpath))
-        run("rm /tmp/{}".format(archive))
-        run("mv {}/web_static/* {}".format(cloudpath, cloudpath))
-        run("rm -rf {}/web_static".format(cloudpath))
-        run("rm -rf {}".format(current))
-        run("ln -s {} {}".format(cloudpath, current))
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
         print('New version deployed!')
         return True
-    else:
+    except:
         return False
 
 
 def deploy():
-    """cript (based on the file 2-do_deploy_web_static.py) that creates
-    and distributes an archive to your web servers.
-    """
-    pack = do_pack()
-    deploy_all = do_deploy(pack)
-    return deploy_all
+    archive_path = do_pack()
+    answer = do_deploy(archive_path)
+    return answer
